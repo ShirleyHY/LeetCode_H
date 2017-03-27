bad60fc2ec50fbe5e4fd5252480b39704bb7cb4f
ShirleyHY <276605911@qq.com>
2017-03-27 09:48:22 +0800

    Update 1_TwoSum.java

--- 1_TwoSum.java
+++ 1_TwoSum.java
@@ -1,3 +1,10 @@
+/*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result=new int[2];
