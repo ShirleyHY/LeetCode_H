0fbeb85c1c473b4db7af63df34764fb22614644c
ShirleyHY <276605911@qq.com>
2017-03-27 09:40:12 +0800

    Update 1_TwoSum.java

--- 1_TwoSum.java
+++ 1_TwoSum.java
@@ -1,1 +1,15 @@
-
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result=new int[2];
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>(); //Map容器将key与value一一映射。本质来说就是将数组的下标和值对调了。
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                result[1]=i;
+                result[0]=map.get(target-nums[i]);
+                return result;
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+}
