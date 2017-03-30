566a4041fe6b0ffc82d475cc665a30d8b308b428
ShirleyHY <276605911@qq.com>
2017-03-30 17:00:29 +0800

    Update RemoveDuplicatesfromSortedArrayII.java

--- RemoveDuplicatesfromSortedArrayII.java
+++ RemoveDuplicatesfromSortedArrayII.java
@@ -37,3 +37,13 @@ public class Solution {
         return count+1;
     }
 }//最简单的想法。但是速度很慢
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+    int i = 0;
+    for (int n : nums)
+        if (i < 2 || n > nums[i-2])
+            nums[i++] = n;
+    return i;
+    }
+}
